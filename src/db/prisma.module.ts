0327037a1fed8f1b581bd2f939f8fb11434b0d2e
nanahandresaputra <nanaggmu@gmail.com>
2025-10-14 15:23:26 +0700

    change inport PrismaClient src/db/ to ../db -> 2

--- src/db/prisma.module.ts
+++ src/db/prisma.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 
-import { PrismaService } from 'src/db/prisma.service';
-
+// import { PrismaService } from 'src/db/prisma.service'; // local
+import { PrismaService } from './prisma.service'; // prod -> vercel
 @Global()
 @Module({
   providers: [PrismaService],
