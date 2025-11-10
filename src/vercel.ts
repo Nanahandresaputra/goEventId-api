31aedcc52972b776908117ca237def4b1a1fb524
nanahandresaputra <nanaggmu@gmail.com>
2025-11-10 23:52:21 +0700

    update

---
+++ src/vercel.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json } from 'body-parser';
+
+let appInstance: any;
+
+export default async function handler(req: any, res: any) {
+  if (!appInstance) {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('goEventId/api/v1');
+    app.use(json({ limit: '5mb' }));
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    await app.init();
+    appInstance = app.getHttpAdapter().getInstance();
+  }
+
+  return appInstance(req, res);
+}
